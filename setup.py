52ee377e78622aaf2670865f191e666a9219a4b6
Jung Gyu Yoon <yjg30737@gmail.com>
2022-07-23 17:01:40 +0900

    Update setup.py to upload this to pypi

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
 
 setup(
     name='pyqt-find-path-widget',
@@ -10,6 +17,8 @@ setup(
     package_data={'pyqt_find_path_widget.style': ['button.css', 'lineedit.css']},
     description='PyQt find path widget (QLabel - QLineEdit - QPushButton)',
     url='https://github.com/yjg30737/pyqt-find-path-widget.git',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
     install_requires=[
         'PyQt5>=5.8',
         'pyqt-resource-helper>=0.0.1'
